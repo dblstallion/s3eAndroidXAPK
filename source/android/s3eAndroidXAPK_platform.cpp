a9fee6039dd10d1e27c39b7cf29c5d7cb918bd1b
Daniel Menard <dan@crankshaftgames.com>
2013-12-03 17:02:41 -0500

    Began JNI for the helper classes. Added Callback registration code

--- source/android/s3eAndroidXAPK_platform.cpp
+++ source/android/s3eAndroidXAPK_platform.cpp
@@ -1,11 +1,11 @@
 /*
- * android-specific implementation of the s3eAndroidXAPK extension.
- * Add any platform-specific functionality here.
- */
+* android-specific implementation of the s3eAndroidXAPK extension.
+* Add any platform-specific functionality here.
+*/
 /*
- * NOTE: This file was originally written by the extension builder, but will not
- * be overwritten (unless --force is specified) and is intended to be modified.
- */
+* NOTE: This file was originally written by the extension builder, but will not
+* be overwritten (unless --force is specified) and is intended to be modified.
+*/
 #include "s3eAndroidXAPK_internal.h"
 
 #include "s3eEdk.h"
@@ -16,6 +16,13 @@
 static jobject g_Obj;
 static jmethodID g_s3eAndroidXAPKGetFiles;
 
+static jfieldID g_fileName;
+static jfieldID g_fileUrl;
+static jfieldID g_fileSize;
+
+static jfieldID g_responseResult;
+static jfieldID g_responseFiles;
+
 void responseReceived(JNIEnv* env, jobject obj, jobject response);
 
 s3eResult s3eAndroidXAPKInit_platform()
@@ -24,6 +31,8 @@ s3eResult s3eAndroidXAPKInit_platform()
     JNIEnv* env = s3eEdkJNIGetEnv();
     jobject obj = NULL;
     jmethodID cons = NULL;
+    jclass responseCls = NULL;
+    jclass fileCls = NULL;
 
     // Get the extension class
     jclass cls = s3eEdkAndroidFindClass("s3eAndroidXAPK");
@@ -48,6 +57,37 @@ s3eResult s3eAndroidXAPKInit_platform()
 
     g_s3eAndroidXAPKGetFiles = env->GetMethodID(cls, "s3eAndroidXAPKGetFiles", "(Ljava/lang/String;[B;I)V");
     if (!g_s3eAndroidXAPKGetFiles)
+        goto fail;
+
+    // Get the helper classes
+    fileCls = s3eEdkAndroidFindClass("s3eAndroidXAPK$File");
+    if (!fileCls)
+        goto fail;
+
+    
+    responseCls = s3eEdkAndroidFindClass("s3eAndroidXAPK$Response");
+    if (!responseCls)
+        goto fail;
+
+    // Get the fields
+    g_fileName = env->GetFieldID(fileCls, "name", "Ljava/lang/String;");
+    if (!g_fileName)
+        goto fail;
+
+    g_fileUrl = env->GetFieldID(fileCls, "url", "Ljava/lang/String;");
+    if (!g_fileUrl)
+        goto fail;
+
+    g_fileSize = env->GetFieldID(fileCls, "size", "J");
+    if (!g_fileSize)
+        goto fail;
+
+    g_responseResult = env->GetFieldID(responseCls, "result", "I");
+    if (!g_responseResult)
+        goto fail;
+
+    g_responseFiles = env->GetFieldID(responseCls, "files", "[Ls3eAndroidXAPK$File;");
+    if (!g_responseFiles)
         goto fail;
 
     IwTrace(ANDROIDXAPK, ("ANDROIDXAPK init success"));
@@ -77,17 +117,17 @@ void s3eAndroidXAPKTerminate_platform()
 
 s3eResult s3eAndroidXAPKRegister_platform(s3eAndroidXAPKCallback callbackID, s3eCallback callbackFn, void* userData)
 {
-    return S3E_RESULT_ERROR;
+    return s3eEdkCallbacksRegister(S3E_EXT_ANDROIDXAPK_HASH, s3eAndroidXAPKCallback_MAX, callbackID, callbackFn, userData, false);
 }
 
 s3eResult s3eAndroidXAPKUnRegister_platform(s3eAndroidXAPKCallback callbackID, s3eCallback callbackFn)
 {
-    return S3E_RESULT_ERROR;
+    return s3eEdkCallbacksUnRegister(S3E_EXT_ANDROIDXAPK_HASH, s3eAndroidXAPKCallback_MAX, callbackID, callbackFn);
 }
 
 s3eResult s3eAndroidXAPKGetFiles_platform(const char* base64PublicKey, const void* salt, int32 saltLength)
 {
-    if(saltLength != 20)
+    if (saltLength != 20)
     {
         s3eEdkErrorSet(S3E_EXT_ANDROIDXAPK_HASH, 1, S3E_EXT_ERROR_PRI_NORMAL);
         s3eEdkErrorSetString("Salt must be 20 bytes long");
@@ -101,6 +141,8 @@ s3eResult s3eAndroidXAPKGetFiles_platform(const char* base64PublicKey, const voi
     env->CallVoidMethod(g_Obj, g_s3eAndroidXAPKGetFiles, base64PublicKey_jstr, salt_jba);
     env->DeleteLocalRef(salt_jba);
     env->DeleteLocalRef(base64PublicKey_jstr);
+
+    return S3E_RESULT_SUCCESS;
 }
 
 void responseReceived(JNIEnv* env, jobject obj, jobject response)
